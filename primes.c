6010e0c0e2a24ae6d0c4c44ccf55a1c325e90958
Wayne Diamond <Wayne@Waynes-cool-machine.local>
2018-08-26 10:39:58 +0800

    string

--- primes.c
+++ primes.c
@@ -3,16 +3,16 @@
 
 int main(void)
 {
-    int p, i, primes[50], primeIndex = 2;
+    int p, i, primes[50], primeIndex = 2; // p:当前奇数
     bool isPrime;
 
     primes[0] = 2;
     primes[1] = 3;
 
-    for(p = 5; p <= 50; p += 2){
+    for(p = 5; p <= 50; p += 2){ // 从5开始 遍历奇数 5 7 9 ...
         isPrime = true;
 
-        for(i = 1; isPrime && p / primes[i] >= primes[i]; ++i)
+        for(i = 1; isPrime && (p / primes[i] >= primes[i]); ++i)
             if(p % primes[i] == 0) isPrime = false;
 
         if(isPrime == true){
---
+++ string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* 合并两个字符串 n1 n2 是长度 */
+void concat(char result[], char str1[], int n1, char str2[], int n2)
+{
+    while(*str1)
+        *result++ = *str1++;
+
+    *result++ = ' ';
+
+    while(*str2) {
+        printf("%c\n", *str2);
+        *result++ = *str2++;
+    }
+
+    *result = '\0';
+}
+
+int main(void)
+{
+    char s1[] = "hello"; // 单引号char 双引号string
+    char s2[] = "world";
+    char s3[20];
+
+    concat(s3, s1, 5, s2, 5);
+    printf("s3: %s\n", s3);
+}
